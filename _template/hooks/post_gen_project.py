670d051b53f4e108a74fc2f4a8d759ae21834179
Stephen Kent <smkent@smkent.net>
2024-08-02 14:19:51 -0700

    Locate repo root path in template project hook

--- _template/hooks/post_gen_project.py
+++ _template/hooks/post_gen_project.py
@@ -1,5 +1,11 @@
 from pathlib import Path
 
+repo_path = Path("..")
+while not (repo_path / ".git").is_dir():
+    if (next_path := repo_path / "..").resolve() == repo_path.resolve():
+        raise Exception("Unable to locate repository root")
+    repo_path = next_path
+
 # Create project library table symlinks
 for link_name in ["fp-lib-table", "sym-lib-table"]:
-    (Path(".") / link_name).symlink_to(Path("..") / "libraries" / link_name)
+    (Path(".") / link_name).symlink_to(repo_path / "libraries" / link_name)
